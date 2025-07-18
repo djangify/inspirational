26184b6299d0592587a2dbde5f259fe65ee1a1a4
djangify <djangify@gmail.com>
2025-07-18 09:43:10 +0100

    Update footer and header. Add about and contact page. Include prompt generator page. Moved policy documents

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import homepage
 from . import views
 
+app_name = "core"
+
 urlpatterns = [
     path("", homepage, name="homepage"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
     path("policy/privacy/", views.privacy_view, name="privacy_policy"),
     path("policy/cookies/", views.cookie_view, name="cookie_policy"),
     path("policy/contents/", views.content_view, name="content_policy"),
--- core/views.py
+++ core/views.py
@@ -23,3 +23,25 @@ def cookie_view(request):
 def content_view(request):
     template_name = "policy/contents.html"
     return render(request, template_name)
+
+
+def about(request):
+    """
+    View for the About page
+    """
+    context = {
+        "title": "About Inspirational Guidance",
+        "meta_description": "Inspirational Guidance provides self-help guides and teach practical life skills",
+    }
+    return render(request, "core/about.html", context)
+
+
+def contact(request):
+    """
+    View for the contact page
+    """
+    context = {
+        "title": "Contact Inspirational Guidance",
+        "meta_description": "We provide self-help guides and teach practical life skills",
+    }
+    return render(request, "core/contact.html", context)
