e61708f6c3e1b3d28f34a53de3f774688adb21f8
djangify <djangify@gmail.com>
2025-07-29 12:18:39 +0100

    Amend url for blog posts to remove /news/

--- inspirational/urls.py
+++ inspirational/urls.py
@@ -22,10 +22,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
     path("shop/", include("shop.urls", namespace="shop")),
-    path("news/", include("news.urls", namespace="news")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
     path("prompt/", include("prompt.urls")),
     path("tools/", include("tools.urls", namespace="tools")),
+    path("", include("news.urls", namespace="news")),
     # Sitemap and robots.txt
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
     path(
--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "news"
 
 urlpatterns = [
-    path("", views.news_list, name="list"),
+    path("blog/", views.news_list, name="list"),
     path("category/<slug:slug>/", views.category_list, name="category"),
     path("<slug:slug>/", views.post_detail, name="detail"),
 ]
