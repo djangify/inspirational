e490d4fa96d20b862e07a4b14c5aa5a363c97dc9
djangify <djangify@gmail.com>
2025-08-13 10:00:45 +0100

    Add robots.txt file

--- inspirational/urls.py
+++ inspirational/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path(
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+        name="robots_txt",
     ),
 ]
 if settings.DEBUG:
