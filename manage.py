09f76207664a92e60ca28dc6c532a6673e88065e
Diane Corriette <131492009+todiane@users.noreply.github.com>
2026-01-30 18:37:47 +0000

    Change settings folder to settings.py file ready for Docker. Add Docker files. Upload json files from production

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inspirational.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inspirational.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
